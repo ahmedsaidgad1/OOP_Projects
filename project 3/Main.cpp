19374ec8d1ad31bd0882207ce1a402748587e300
Ahmed Gad <ahmed.said.gad1@gmail.com>
2026-07-02 02:12:18 +0300

    Add Input and Validation Library and enhance clsBank_Client with update functionality

--- project 3/Main.cpp
+++ project 3/Main.cpp
@@ -1,18 +1,8 @@
-#include<iostream>
-#include"clsBank_Client.h"
+#include <iostream>
+#include "clsBank_Client.h"
+
 int main()
 {
-    clsBank_Client Client1 = clsBank_Client::Find("A07", "125");
-
-    if (Client1.Is_Empty())
-    {
-        cout << "This PIN code is not found for any client."<<endl;;
-    }
-    else
-    {
-        cout << "This PIN code exists."<<endl;
-        Client1.Print();
-    }
-
+    clsBank_Client::Update_Client();
     return 0;
-}       
+}
--- project 3/clsBank_Client.h
+++ project 3/clsBank_Client.h
@@ -5,7 +5,7 @@
 #include <fstream>
 #include "clsString.h"
 #include "clsPerson.h"
-#include <string>
+#include "Input_and_Validation_Library.h"
 using namespace std;
 
 class clsBank_Client : public clsPerson
@@ -140,6 +140,117 @@ public:
         }
         return _Get_Empty_Client_Object();
     }
+    static bool Is_Account_Exist(string Account_Number)
+    {
+        clsBank_Client Client = Find(Account_Number);
+        return (!Client.Is_Empty());
+    }
+
+    enum enSave_Results
+    {
+        svSucceeded = 0,
+        svFaildEmptyObject = 1,
+        svFaildAccountFound = 2
+    };
+
+    enSave_Results Save()
+    {
+        if (Is_Empty())
+            return enSave_Results::svFaildEmptyObject;
+
+        if (!Is_Account_Exist(_Account_Number))
+            return enSave_Results::svFaildAccountFound;
+
+        Save_Update();
+        return enSave_Results::svSucceeded;
+    }
+
+    void Save_Update()
+    {
+        vector<clsBank_Client> vClients;
+        fstream My_File;
+        My_File.open("Clients.txt", ios::in);
+        if (My_File.is_open())
+        {
+            string Line;
+            while (getline(My_File, Line))
+            {
+                clsBank_Client Client = _Convert_Line_To_Client_Object(Line);
+                if (Client._Account_Number == _Account_Number)
+                {
+                    Client = *this;
+                }
+                vClients.push_back(Client);
+            }
+            My_File.close();
+        }
+
+        My_File.open("Clients.txt", ios::out);
+        if (My_File.is_open())
+        {
+            for (clsBank_Client Client : vClients)
+            {
+                My_File << _Convert_Client_Object_To_Line(Client) << endl;
+            }
+            My_File.close();
+        }
+    }
+
+    static void Update_Client()
+    {
+        string Account_Number = "";
+        cout << "Enter Account Number: ";
+        Account_Number = cls_Input_Validation::Read_string();
+        while (!clsBank_Client::Is_Account_Exist(Account_Number))
+        {
+            cout << "Account Number is not found, Enter another Account Number: ";
+            Account_Number = cls_Input_Validation::Read_string();
+        }
+        clsBank_Client Client1 = clsBank_Client::Find(Account_Number);
+        Client1.Print();
+        cout << "Update Client Info: " << endl;
+        cout << "-----------------------------" << endl;
+        Read_Client_Info(Client1);
+        clsBank_Client::enSave_Results Save_Result;
+        Save_Result = Client1.Save();
+        switch (Save_Result)
+        {
+        case clsBank_Client::enSave_Results::svSucceeded:
+        {
+            cout << "Account Updated Successfully." << endl;
+            Client1.Print();
+            break;
+        }
+
+        case clsBank_Client::enSave_Results::svFaildEmptyObject:
+        {
+            cout << "Failed to update account. Invalid client object." << endl;
+            break;
+        }
+        case clsBank_Client::enSave_Results::svFaildAccountFound:
+        {
+            cout << "Failed to update account. Account not found." << endl;
+            break;
+        }
+        }
+    }
+
+    static void Read_Client_Info(clsBank_Client &Client)
+    {
+        cout << "Enter First Name: ";
+        Client.Set_First_Name(cls_Input_Validation::Read_string());
+        cout << "Enter Last Name: ";
+        Client.Set_Last_Name(cls_Input_Validation::Read_string());
+        cout << "Enter Email: ";
+        Client.Set_Email(cls_Input_Validation::Read_string());
+        cout << "Enter Phone: ";
+        Client.Set_Phone(cls_Input_Validation::Read_string());
+        cout << "Enter Pin Code: ";
+        Client.SetPinCode(cls_Input_Validation::Read_string());
+        cout << "Enter Account Balance: ";
+        Client.Set_Account_Balance(cls_Input_Validation::Read_Float_Number());
+    }
+
     void Print()
     {
         cout << endl;
